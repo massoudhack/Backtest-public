c31873a7c1a05fc91498beaf55c9f245b64afe4e
massoudhack <69686207+massoudhack@users.noreply.github.com>
2026-09-13 20:23:58 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,8 @@
-const CACHE = 'tj-cache-v11';
-const ASSETS = [
-  './',
-  './index.html',
-  './trades_data.js',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (e) => {
+const CACHE = 'tj-cache-v12';
+const ASSETS = ['./','./index.html','./trades_data.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(res => {
-      try {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
-      } catch(err) {}
-      return res;
-    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
-  );
+  e.respondWith(fetch(e.request).then(res => { try { const copy = res.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{}); } catch(err){} return res; }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html'))));
 });
